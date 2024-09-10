72643e2834c0ed707ad0f56bf563b166cda1d870
Hiiiua <mhua4816@gmail.com>
2024-09-10 17:22:39 -0500

    limited classification dataset and alexnet

---
+++ classifications/alexnet/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import json
+
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets, utils
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+from tqdm import tqdm
+
+from model import AlexNet
